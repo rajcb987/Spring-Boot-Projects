56391458b9eb2f9ed2be2e8edcf201bc40a4b2ce
RAJ <48962069+rajcb987@users.noreply.github.com>
2024-05-14 12:06:44 +0530

    Create SpringBootFrameWorkApplication.java

---
+++ SpringBootFrameWorkApplication.java
@@ -0,0 +1,14 @@
+package com.abcde.springbootframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootFrameworkApplication.class, args);
+		
+	}
+
+}
