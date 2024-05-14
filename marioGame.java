3da01b97760a0f2f5c11f0508c0d8b83a91937ee
RAJ <48962069+rajcb987@users.noreply.github.com>
2024-05-14 12:07:41 +0530

    Create marioGame.java

---
+++ marioGame.java
@@ -0,0 +1,25 @@
+package com.abcde.springbootframework.game;
+
+
+public class marioGame {
+
+	public void up()
+	{
+		System.out.println("Jump");
+	}
+	
+	public void down()
+	{
+		System.out.println("Get into the Hole");
+	}
+	
+	public void left()
+	{
+		System.out.println("Move Back");
+	}
+	
+	public void right()
+	{
+		System.out.println("Accelerate");
+	}
+}
